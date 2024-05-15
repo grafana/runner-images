d7fdf801599cfb6ec30d86188e9184794e0bf7f3
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 09:57:09 -0300

    Add ARM64 packer template, update scripts for multiple architectures (#2)
    * Add ARM64 packer template, update scripts for multiple architectures
    
    * Update AMI creation instance type
    
    * Remove debug output from software report

--- images/ubuntu/scripts/build/install-powershell.sh
+++ images/ubuntu/scripts/build/install-powershell.sh
@@ -8,6 +8,12 @@
 source $HELPER_SCRIPTS/install.sh
 source $HELPER_SCRIPTS/os.sh
 
+arch=$(get_arch)
+
+if [[ $arch == "amd64" ]]; then
+    arch="x64"
+fi
+
 pwsh_version=$(get_toolset_value .pwsh.version)
 
 # Install Powershell
@@ -17,5 +23,10 @@ if is_ubuntu24; then
     package_path=$(download_with_retry "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/powershell-lts_7.4.2-1.deb_amd64.deb")
     sudo dpkg -i "$package_path"
 else
-    apt-get install powershell=$pwsh_version*
+    # apt-get install powershell=$pwsh_version*
+    package_path=$(download_with_retry https://github.com/PowerShell/PowerShell/releases/download/v$pwsh_version/powershell-$pwsh_version-linux-$arch.tar.gz)
+    mkdir -p /opt/microsoft/powershell/7
+    tar zxf "$package_path" -C /opt/microsoft/powershell/7
+    chmod +x /opt/microsoft/powershell/7/pwsh
+    ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 fi
