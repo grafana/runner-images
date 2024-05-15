5eca5f0d59f7cf7568130cbccd318ab265374e80
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 09:57:09 -0300

    Add ARM64 packer template, update scripts for multiple architectures (#2)
    * Add ARM64 packer template, update scripts for multiple architectures
    
    * Update AMI creation instance type
    
    * Remove debug output from software report

--- images/ubuntu/scripts/build/install-miniconda.sh
+++ images/ubuntu/scripts/build/install-miniconda.sh
@@ -6,9 +6,20 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/etc-environment.sh
+source $HELPER_SCRIPTS/os.sh
+
+arch=$(get_arch)
+
+if [[ $arch == "amd64" ]]; then
+    arch="x86_64"
+fi
+
+if [[ $arch == "arm64" ]]; then
+    arch="aarch64"
+fi
 
 # Install Miniconda
-curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh \
+curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$arch.sh -o miniconda.sh \
     && chmod +x miniconda.sh \
     && ./miniconda.sh -b -p /usr/share/miniconda \
     && rm miniconda.sh
