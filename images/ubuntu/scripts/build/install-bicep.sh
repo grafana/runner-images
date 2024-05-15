bd6034c7dd30275564443eca00781f6da8479632
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 09:57:09 -0300

    Add ARM64 packer template, update scripts for multiple architectures (#2)
    * Add ARM64 packer template, update scripts for multiple architectures
    
    * Update AMI creation instance type
    
    * Remove debug output from software report

--- images/ubuntu/scripts/build/install-bicep.sh
+++ images/ubuntu/scripts/build/install-bicep.sh
@@ -6,9 +6,16 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
+
+arch=$(get_arch)
+
+if [[ $arch == "amd64" ]]; then
+    arch="x64"
+fi
 
 # Install Bicep CLI
-download_url=$(resolve_github_release_asset_url "Azure/bicep" "endswith(\"bicep-linux-x64\")" "latest")
+download_url=$(resolve_github_release_asset_url "Azure/bicep" "endswith(\"bicep-linux-$arch\")" "latest")
 bicep_binary_path=$(download_with_retry "${download_url}")
 
 # Mark it as executable
