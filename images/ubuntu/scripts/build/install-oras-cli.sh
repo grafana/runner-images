f111b8b7beffd6eb0e1075fa6c6a64ea6ea1a9ea
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 09:57:09 -0300

    Add ARM64 packer template, update scripts for multiple architectures (#2)
    * Add ARM64 packer template, update scripts for multiple architectures
    
    * Update AMI creation instance type
    
    * Remove debug output from software report

--- images/ubuntu/scripts/build/install-oras-cli.sh
+++ images/ubuntu/scripts/build/install-oras-cli.sh
@@ -7,16 +7,19 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
+
+arch=$(get_arch)
 
 # Determine latest ORAS CLI version
-download_url=$(resolve_github_release_asset_url "oras-project/oras" "endswith(\"linux_amd64.tar.gz\")" "latest")
+download_url=$(resolve_github_release_asset_url "oras-project/oras" "endswith(\"linux_$arch.tar.gz\")" "latest")
 
 # Download ORAS CLI
 archive_path=$(download_with_retry "$download_url")
 
 # Supply chain security - ORAS CLI
 hash_url=$(resolve_github_release_asset_url "oras-project/oras" "endswith(\"checksums.txt\")" "latest")
-external_hash=$(get_checksum_from_url "${hash_url}" "linux_amd64.tar.gz" "SHA256")
+external_hash=$(get_checksum_from_url "${hash_url}" "linux_$arch.tar.gz" "SHA256")
 use_checksum_comparison "$archive_path" "${external_hash}"
 
 # Unzip ORAS CLI
