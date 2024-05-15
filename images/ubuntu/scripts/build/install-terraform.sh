1799637f7f497b09eae4bf7dc1e86c697af254fd
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 09:57:09 -0300

    Add ARM64 packer template, update scripts for multiple architectures (#2)
    * Add ARM64 packer template, update scripts for multiple architectures
    
    * Update AMI creation instance type
    
    * Remove debug output from software report

--- images/ubuntu/scripts/build/install-terraform.sh
+++ images/ubuntu/scripts/build/install-terraform.sh
@@ -5,9 +5,12 @@
 ################################################################################
 
 source $HELPER_SCRIPTS/install.sh
+source $HELPER_SCRIPTS/os.sh
+
+arch=$(get_arch)
 
 # Install Terraform
-download_url=$(curl -fsSL https://api.releases.hashicorp.com/v1/releases/terraform/latest | jq -r '.builds[] | select((.arch=="amd64") and (.os=="linux")).url')
+download_url=$(curl -fsSL https://api.releases.hashicorp.com/v1/releases/terraform/latest | jq -r '.builds[] | select((.arch=="'"$arch"'") and (.os=="linux")).url')
 archive_path=$(download_with_retry "${download_url}")
 unzip -qq "$archive_path" -d /usr/local/bin
 
