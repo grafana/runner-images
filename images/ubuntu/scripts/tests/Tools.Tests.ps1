e0f138e0910b590478e04b37a8c8d25be6bd2988
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-15 23:00:12 -0300

    Fix minor issues with software report and tests

--- images/ubuntu/scripts/tests/Tools.Tests.ps1
+++ images/ubuntu/scripts/tests/Tools.Tests.ps1
@@ -106,7 +106,7 @@ Describe "Docker images" {
     }
 }
 
-Describe "Docker-compose v1" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "Docker-compose v1" -Skip:((-not (Test-IsAmd64)) -or ((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22)))) {
     It "docker-compose" {
         "docker-compose --version"| Should -ReturnZeroExitCode
     }
@@ -189,13 +189,13 @@ Describe "MSSQLCommandLineTools" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Tes
     }
 }
 
-Describe "SqlPackage" -Skip:((-not (Test-IsAmd64)) -or (-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "SqlPackage" -Skip:((-not (Test-IsAmd64)) -or ((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22)))) {
     It "sqlpackage" {
         "sqlpackage /version" | Should -ReturnZeroExitCode
     }
 }
 
-Describe "R" -Skip:((-not (Test-IsAmd64)) -or (-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "R" -Skip:((-not (Test-IsAmd64)) -or ((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22)))) {
     It "r" {
         "R --version" | Should -ReturnZeroExitCode
     }
@@ -252,7 +252,7 @@ Describe "Git-lfs" {
     }
 }
 
-Describe "Heroku" -Skip:((-not (Test-IsAmd64)) -or (-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "Heroku" -Skip:((-not (Test-IsAmd64)) -or ((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22)))) {
     It "heroku" {
         "heroku --version" | Should -ReturnZeroExitCode
     }
