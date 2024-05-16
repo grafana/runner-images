c9827ff34caeae52efdf81f379dbcbd8293ac586
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-05-16 11:49:39 -0300

    Fix AMIs being deregisterd and minor issues after upstream merge (#5)
    * Use -Skip instead of conditions to skip tests
    
    * Parametrize force_deregister option
    
    * Fix minor issues with software report and tests
    
    * Update Ubuntu2204 readmes

--- images/ubuntu/scripts/tests/Browsers.Tests.ps1
+++ images/ubuntu/scripts/tests/Browsers.Tests.ps1
@@ -8,36 +8,34 @@ Describe "Firefox" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))
     }
 }
 
-if (Test-IsAmd64) {
-    Describe "Chrome" {
-        It "Chrome" {
-            "google-chrome --version" | Should -ReturnZeroExitCode
-        }
-
-        It "Chrome Driver" {
-            "chromedriver --version" | Should -ReturnZeroExitCode
-        }
-
-        It "Chrome and Chrome Driver major versions are the same" {
-            $chromeMajor = (google-chrome --version).Trim("Google Chrome ").Split(".")[0]
-            $chromeDriverMajor = (chromedriver --version).Trim("ChromeDriver ").Split(".")[0]
-            $chromeMajor | Should -BeExactly $chromeDriverMajor
-        }
+Describe "Chrome" -Skip:(-not (Test-IsAmd64)) {
+    It "Chrome" {
+        "google-chrome --version" | Should -ReturnZeroExitCode
     }
 
-    Describe "Edge" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
-        It "Edge" {
-            "microsoft-edge --version" | Should -ReturnZeroExitCode
-        }
-
-        It "Edge Driver" {
-            "msedgedriver --version" | Should -ReturnZeroExitCode
-        }
+    It "Chrome Driver" {
+        "chromedriver --version" | Should -ReturnZeroExitCode
     }
 
-    Describe "Chromium" {
-        It "Chromium" {
-            "chromium-browser --version" | Should -ReturnZeroExitCode
-        }
+    It "Chrome and Chrome Driver major versions are the same" {
+        $chromeMajor = (google-chrome --version).Trim("Google Chrome ").Split(".")[0]
+        $chromeDriverMajor = (chromedriver --version).Trim("ChromeDriver ").Split(".")[0]
+        $chromeMajor | Should -BeExactly $chromeDriverMajor
+    }
+}
+
+Describe "Edge" -Skip:((-not (Test-IsAmd64)) -or ((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22)))) {
+    It "Edge" {
+        "microsoft-edge --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Edge Driver" {
+        "msedgedriver --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "Chromium" -Skip:(-not (Test-IsAmd64)) {
+    It "Chromium" {
+        "chromium-browser --version" | Should -ReturnZeroExitCode
     }
 }
