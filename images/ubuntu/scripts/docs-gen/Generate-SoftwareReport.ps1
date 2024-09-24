629ad0fbc4f4ae5f49f44f1d9513261104bb9c09
Guilherme Caulada <guilherme.caulada@grafana.com>
2024-09-24 20:42:04 -0300

    Fix dotnet version regex on software report (#120)

--- images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1
+++ images/ubuntu/scripts/docs-gen/Generate-SoftwareReport.ps1
@@ -247,7 +247,7 @@ $browsersTools.AddHeader("Environment variables").AddTable($(Build-BrowserWebdri
 
 # .NET Tools
 $netCoreTools = $installedSoftware.AddHeader(".NET Tools")
-$netCoreTools.AddToolVersionsListInline(".NET Core SDK", $(Get-DotNetCoreSdkVersions), "^\d+\.\d+\.\d")
+$netCoreTools.AddToolVersionsListInline(".NET Core SDK", $(Get-DotNetCoreSdkVersions), "^\d+\.\d+\.\d+")
 $netCoreTools.AddNodes($(Get-DotnetTools))
 
 # Databases
